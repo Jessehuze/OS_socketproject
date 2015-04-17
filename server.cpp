7058e78db0572b0cee0ef0e8d0a4853950b72645
Jessehuze <jbhf39@mst.edu>
2015-04-17 18:05:31 -0500

    Starting dev on server.cpp using http://www.linuxhowtos.org/C_C++/socket.htm

--- server.cpp
+++ server.cpp
@@ -1,16 +1,29 @@
 //server.cpp (works with client.cpp)
 
+
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>  /* define socket */
 #include <netinet/in.h>  /* define internet socket */
 #include <netdb.h>       /* define internet socket */
+#include <thread>
 
 #define SERVER_PORT 3932
 
+using namespace std;
+
 int main()
 {
-  in
+  int socket_fd, newsocket_fd, portNum;
+  socklen_t clientAddrLen;
+  char buffer[256];
+  struct sockaddr_in serverAddress;
+  vector<struct sockaddr_in> ClientAddresses;
+  
+  
   return 0;
 }
