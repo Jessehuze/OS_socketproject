5b071878371569b6b2a53b0d2a3c7faa97c45a2e
Jesse Hughes <jbhf39@mst.edu>
2015-04-17 10:41:22 -0500

    Added server.cpp

---
+++ server.cpp
@@ -0,0 +1,8 @@
+//server.cpp (works with client.cpp)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>  /* define socket */
+#include <netinet/in.h>  /* define internet socket */
+#include <netdb.h>       /* define internet socket */
