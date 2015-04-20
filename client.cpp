6bc61a95c6b5902eed1a9f1b37e2483c3084d0c7
Ragnyll <justanotherjakeyg@gmail.com>
2015-04-20 16:03:15 -0500

    please fix the errors....

--- client.cpp
+++ client.cpp
@@ -66,8 +66,7 @@ int main()
 	}
 	
 	//get ready to connect the server
-	//struct serverSockAddr = {AF_INET, htons(port)};
-	serverSockAddr = {AF_INET, htons(port)};
+	struct serverSockAddr = {AF_INET, htons(port)};
 	struct hostent* hp; 
 	
     /* get the host */
